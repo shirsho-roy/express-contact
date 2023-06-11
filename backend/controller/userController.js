d7b36e20b8e1446bf97329bf4dd6d777cc32b6c7
shirsho-roy <shirsho.tffs@gmail.com>
2023-06-11 23:36:10 +0530

    User Schema Created

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,34 @@
 const asyncHandler= require("express-async-handler");
-
+const User = require("../models/userModel");
+const bcrypt= require("bcrypt");
 //post request
 const registerUser= asyncHandler(async(req,res)=>{
+    const {username,email,password}= req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const userAvailable=await  User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already registered");
+    }
+    
+    //Hashed pasword
+   const hashedPassword=await bcrypt.hash(password,10);
+   const user = await User.create({
+     username,
+     email,
+     password:hashedPassword,
+   });
+   console.log(user);
+   if(user){
+    res.status(201).json({_id:user.id, email:user.email, });
+   }
+   else{
+    res.status(400);
+    throw new Error("User data not valid");
+   }
     res.json({message: "register the user"});
 });
 
