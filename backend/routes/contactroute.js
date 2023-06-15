1e71995070e37fc10507976a045afca763e415f1
shirsho-roy <shirsho.tffs@gmail.com>
2023-06-16 01:47:51 +0530

    Final Version

--- backend/routes/contactroute.js
+++ backend/routes/contactroute.js
@@ -1,6 +1,10 @@
 const express= require("express");
 const router=express.Router();
-const {getContacts,createContact,getContact,updateContact, deleteContact}=require("../controller/controller.js")
+const {getContacts,createContact,getContact,updateContact, deleteContact}=require("../controller/controller.js");
+const { validateToken } = require("../middlewares/validateTokenHandler.js");
+
+router.use(validateToken);//uses middleware on all the routes
+
 router.route('/').get(getContacts).post(createContact);
 
 router.route('/:id').get(getContact);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express= require("express");
 const router=express.Router();
 const { registerUser, loginUser, currentUser } = require("../controller/userController");
+const { validateToken } = require("../middlewares/validateTokenHandler");
 
 router.route("/register").post(registerUser);
 
 router.route("/login").post(loginUser);
 
-router.route("/current").get(currentUser);
+// router.route("/current").get(currentUser);
+router.get('/current',validateToken,currentUser);
 
 module.exports=router;
